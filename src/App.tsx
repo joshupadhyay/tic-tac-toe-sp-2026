c6e87ea2c2e428e04ac2869e541f2f743f5bb94f
Josh U <joshupad@gmail.com>
2026-02-03 14:55:10 -0500

    gameboard displays, and winning message displays. Need to add game freeze and draw conditions to tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
-import { useState } from "react";
-import { createGame, makeMove } from "./tic-tac-toe";
+import { useEffect, useState } from "react";
+import { createGame, getWinner, makeMove } from "./tic-tac-toe";
+
+import "./Table.css";
 
 function App() {
-  let [gameState, setGameState] = useState(getInitialGame())
+  let [gameState, setGameState] = useState(getInitialGame());
 
   // TODO: display the gameState, and call `makeMove` when a player clicks a button
-  return <div>Hello World! current player: {gameState.currentPlayer}</div>;
+  return (
+    <div>
+      <div className="board">
+        {gameState.board.map((cell, idx) => (
+          <div
+            key={idx}
+            onClick={() => {
+              setGameState(makeMove(gameState, idx));
+            }}
+          >
+            {cell}
+          </div>
+        ))}
+      </div>
+      {getWinner(gameState) != null ? (
+        <>
+          <h1>{`${getWinner(gameState)} WON BABYYYY`}</h1>
+        </>
+      ) : (
+        <p>Hello World! current player: {gameState.currentPlayer}</p>
+      )}
+    </div>
+  );
 }
 
 function getInitialGame() {
-  let initialGameState = createGame()
-  initialGameState = makeMove(initialGameState, 3)
-  initialGameState = makeMove(initialGameState, 0)
-  return initialGameState
+  let initialGameState = createGame();
+  initialGameState = makeMove(initialGameState, 3);
+  initialGameState = makeMove(initialGameState, 0);
+  return initialGameState;
 }
 
 export default App;
--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -23,10 +23,103 @@ export function createGame(): GameState {
   };
 }
 
+// makeMove should get the existing state of the cell, and adjust with the Player's symbol
 export function makeMove(state: GameState, position: number): GameState {
-  return state
+  const currentValue = state.board[position];
+
+  // Replace the state of the board with the currentPlayer's symbol
+
+  if (currentValue != null) {
+    throw Error("Position is already occupied!");
+  }
+
+  if (position > 8 || position < 0) {
+    throw Error("Position must be between 0 and 8");
+  }
+
+  if (!Number.isInteger(position)) {
+    throw Error("Position must be an integer");
+  }
+
+  // First allow current player to make a move
+
+  const newBoard = [...state.board] as Board;
+
+  newBoard[position] = state.currentPlayer;
+
+  const newState = {
+    board: newBoard,
+    currentPlayer: state.currentPlayer,
+  };
+
+  getWinner(newState);
+
+  // return new state
+  return {
+    board: newState.board,
+    currentPlayer: switchPlayer(state),
+  };
 }
 
 export function getWinner(state: GameState): Player | null {
+  if (winDetection(state)) {
+    // since we ret
+    const winner = state.currentPlayer === "X" ? "O" : "X";
+    return winner;
+  }
+
   return null;
+}
+
+/**
+ * Update currentPlayer after a turn
+ * @param state
+ * @returns updated Player State
+ */
+export function switchPlayer(state: GameState): Player {
+  return state.currentPlayer == "O" ? "X" : "O";
+}
+
+/**
+ * After each move, check board for a possible win. Return T/F for is game over
+ * @param state
+ */
+export function winDetection(state: GameState) {
+  const WinningPositions: number[][] = [
+    // main diagonal
+    [0, 4, 8],
+
+    // anti-diagonal
+    [2, 4, 6],
+
+    // right column
+    [2, 5, 8],
+
+    // left column
+    [0, 3, 6],
+
+    // middle column
+    [1, 4, 7],
+
+    // top row
+    [0, 1, 2],
+
+    // middle row
+    [3, 4, 5],
+
+    // bottom row
+    [6, 7, 8],
+  ];
+
+  const player = state.currentPlayer === "X" ? "O" : "X";
+
+  for (let i: number = 0; i < WinningPositions.length; i++) {
+    let possibleWin = WinningPositions[i];
+
+    if (possibleWin.every((position) => state.board[position] == player)) {
+      return true; //every board position from the possible win array is the same symbol = a win
+    }
+  }
+
+  return false;
 }
